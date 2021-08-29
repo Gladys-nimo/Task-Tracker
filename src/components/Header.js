73a378503c8247e57b4e5651a85055695dbaeb03
Gladys-nimo <wkanyora10@gmail.com>
2021-08-29 04:14:38 +0300

    added a proptype

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
+import PropTypes from 'prop-types'
 
-const Header = () => {
+const Header = ({ title }) => {
     return (
        <header>
-           <h1>Task Tracker</h1>
-       </header>
-       
-       
+           <h1>{title}</h1>
+       </header>  
     )
 }
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+// i faced an error because i didnt start with small p//
+
 
 export default Header
